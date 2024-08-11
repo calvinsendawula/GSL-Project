d13517a8f267a73814af64064144a8fb70f1814c
calvinsendawula <calvin.sendawula@strathmore.edu>
2024-08-11 22:15:33 +0300

    feat: update data ingestion pipeline

--- src/gslTranslater/components/data_ingestion.py
+++ src/gslTranslater/components/data_ingestion.py
@@ -1,5 +1,5 @@
 import os
-import zipfile
+import tarfile
 import gdown
 from gslTranslater import logger
 from gslTranslater.utils.common import get_size
@@ -9,36 +9,24 @@ class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
 
-    
-    def download_file(self)-> str:
-        '''
-        Fetch data from the url
-        '''
-
-        try: 
+    def download_file(self) -> str:
+        try:
             dataset_url = self.config.source_URL
-            zip_download_dir = self.config.local_data_file
+            tar_download_dir = self.config.local_data_file
             os.makedirs("artifacts/data_ingestion", exist_ok=True)
-            logger.info(f"Downloading data from {dataset_url} into file {zip_download_dir}")
+            logger.info(f"Downloading data from {dataset_url} into file {tar_download_dir}")
 
             file_id = dataset_url.split("/")[-2]
             prefix = 'https://drive.google.com/uc?/export=download&id='
-            gdown.download(prefix+file_id,zip_download_dir)
+            gdown.download(prefix + file_id, tar_download_dir, quiet=False)
 
-            logger.info(f"Downloaded data from {dataset_url} into file {zip_download_dir}")
+            logger.info(f"Downloaded data from {dataset_url} into file {tar_download_dir}")
 
         except Exception as e:
             raise e
-        
-    
 
-    def extract_zip_file(self):
-        """
-        zip_file_path: str
-        Extracts the zip file into the data directory
-        Function returns None
-        """
-        unzip_path = self.config.unzip_dir
-        os.makedirs(unzip_path, exist_ok=True)
-        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
-            zip_ref.extractall(unzip_path)
+    def extract_tar_file(self):
+        extract_path = self.config.unzip_dir
+        os.makedirs(extract_path, exist_ok=True)
+        with tarfile.open(self.config.local_data_file, 'r:gz') as tar_ref:
+            tar_ref.extractall(extract_path)
